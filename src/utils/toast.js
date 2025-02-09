3eb76d9a15bd88e7b3920340a69edffbc2335756
Anshad <mkanshadofficial@gmail.com>
2025-02-09 19:32:04 +0530

    git merged with krishna's update

---
+++ src/utils/toast.js
@@ -0,0 +1,24 @@
+import { toast } from 'react-toastify';
+
+export const showToast = {
+  success: (message) => {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  },
+  error: (message) => {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  }
+}; 
