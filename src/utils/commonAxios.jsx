9a9e134e8112bf2288478760461ad9f9e6520610
Anshad.MK <anshad.liquidlab@gmail.com>
2026-04-13 00:49:32 +0530

    chore: update VITE_BASEURL to production endpoint and enable dynamic configuration in commonAxios

--- src/utils/commonAxios.jsx
+++ src/utils/commonAxios.jsx
@@ -5,8 +5,8 @@ import { toast } from 'react-toastify';
 import React from 'react';
 // Create axios instance with base configuration
 const commonAxios = axios.create({
-  // baseURL: import.meta.env.VITE_BASEURL,
-  baseURL: '/api/',
+  baseURL: import.meta.env.VITE_BASEURL,
+  // baseURL: '/api/',
   withCredentials: true,
 });
 
